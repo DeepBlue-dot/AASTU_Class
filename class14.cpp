8bdb3c820f14dd27fd917810480d7a7fe23ed0a4
DeepBlue-dot <deepblue5991@gmail.com>
2023-03-17 03:49:38 +0300

    class

---
+++ class14.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int i = 10; i < 100; i += 10)
+    {
+        for(int j = i; j < i+10; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
